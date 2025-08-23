63a0402c911b55c65f012ccd5afb9970890acf22
xyx <876858298@qq.com>
2025-08-23 23:29:51 +0800

    1

--- autoTrade/selection_manager/service/selection_service.py
+++ autoTrade/selection_manager/service/selection_service.py
@@ -273,7 +273,7 @@ class SelectionService:
         # --- 1. 参数声明 ---
         # D(t) 相关参数
         H_LOOKBACK_K = 5      # h(t,k) 的最大回溯期 k
-        A_PARAM = 1000.0      # tanh 激活函数的缩放系数
+        A_PARAM = 200.0      # tanh 激活函数的缩放系数
         # I(t) 相关参数
         ADX_PERIOD = 14       # ADX 计算周期
         ADX_THRESHOLD = 20.0  # ADX 强度阈值
